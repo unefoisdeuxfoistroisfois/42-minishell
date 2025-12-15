778b4c597acc5f78a08d9c8668c2c33b68d8183a
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-12-15 22:21:41 +0100

    Implementation du ps1

--- main.c
+++ main.c
@@ -6,21 +6,30 @@
 /*   By: britela- <britela-@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 16:12:28 by britela-          #+#    #+#             */
-/*   Updated: 2025/12/15 16:17:41 by britela-         ###   ########.fr       */
+/*   Updated: 2025/12/15 22:18:22 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main()
+void	ft_ps1()
+{
+	char	*line;
+
+	line = readline("minibiendur$ ");
+	free(line);
+}
+
+int	main(int argc, char **argv)
 {
 	int	ninfini;
+	(void)argc;
+	(void)argv;
 
 	ninfini = 1;
 	while(ninfini)
 	{
-		printf("MINISHELL");
-		/* Commande à lancé */
+		ft_ps1();
 	}
 	return (0);
 }
