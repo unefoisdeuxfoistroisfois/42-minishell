a431ab4356566895ccc5f839bfd9f964a092bf80
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-12-15 22:22:43 +0100

    Ajout de bibliotheque

--- minishell.h
+++ minishell.h
@@ -6,13 +6,19 @@
 /*   By: britela- <britela-@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 16:12:58 by britela-          #+#    #+#             */
-/*   Updated: 2025/12/15 16:17:06 by britela-         ###   ########.fr       */
+/*   Updated: 2025/12/15 21:59:48 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// main.c
+void	ft_ps1();
 
 #endif
