368245fcddaa50a117b4abff6a8037d770e6cb3b
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-12-22 21:25:18 +0100

    Ajout d'un fichier a part

--- ps1.c
+++ ps1.c
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.42belgium.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 16:35:32 by britela-          #+#    #+#             */
-/*   Updated: 2025/12/16 16:36:04 by britela-         ###   ########.fr       */
+/*   Updated: 2025/12/22 21:18:48 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,13 @@ void	ft_ps1()
 	char	*line;
 
 	line = readline("minibiendur$ ");
+	if (line == NULL)
+	{
+		return ;
+	}
+	if (line[0] != '\0')
+	{
+		add_history(line);
+	}
 	free(line);
 }
