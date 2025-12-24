501450053a6d52115bca8e5c7030bda43bd631fc
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-12-24 22:31:28 +0100

    Protoype ajouté

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.42belgium.be>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 16:12:58 by britela-          #+#    #+#             */
-/*   Updated: 2025/12/16 16:36:31 by britela-         ###   ########.fr       */
+/*   Updated: 2025/12/24 22:27:21 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@
 
 // ps1.c
 void	ft_ps1();
+
+// args.c
+void	ft_lexer(char *line);
+int	ft_space(char *str, int index);
+int	ft_word(char *str, int index);
 
 #endif
