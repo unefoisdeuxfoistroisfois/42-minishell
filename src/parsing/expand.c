d539192438ee420f4cb00e0229386aa5558d1adf
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-23 15:25:14 +0100

    expand

--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -1,5 +1,60 @@
 #include "minishell.h"
 
+char	*ft_expand_str(char *str)
+{
+	char	*result;
+	char	*var_value;
+	char	quote;
+	int		i;
+	int		len;
+
+	result = ft_strdup("");
+	quote = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] == '\'' || str[i] == '"') && quote == 0)
+			quote = str[i];
+		else if (str[i] == quote)
+			quote = 0;
+		if (str[i] == '$' && str[i + 1] != '\0' && quote != '\'')
+		{
+			len = ft_var_len(str, i + 1);
+			var_value = ft_get_var_value(str, i + 1, len);
+			result = ft_strjoin_free(result, var_value);
+			free(var_value);
+			i = i + len + 1;
+		}
+		else
+		{
+			result = ft_addchar(result, str[i]);
+			i++;
+		}
+	}
+	return (result);
+}
+char	*ft_get_var_value(char *str, int start, int len)
+{
+	char	*var_name;
+	char	*value;
+	char	*res;
+
+	var_name = ft_substr(str, start, len);
+	if (var_name == NULL)
+	{
+		return (NULL);
+	}
+	value = getenv(var_name);
+	free(var_name);
+	if (value == NULL)
+	{
+		res = ft_strdup("");
+		return (res);
+	}
+	res = ft_strdup(value);
+	return (res);
+}
+
 int	ft_var_len(char *str, int start)
 {
 	int	len;
