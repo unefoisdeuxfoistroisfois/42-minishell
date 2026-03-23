07151d721b4166943462014e0bd25157a264a442
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-23 12:48:34 +0100

    Expand

--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -1,5 +1,26 @@
 #include "minishell.h"
 
+int	ft_var_len(char *str, int start)
+{
+	int	len;
+	int	bool;
+
+	len = 0;
+	// $? est un cas spécial
+	if (str[start] == '?')
+		return (1);
+	bool = 1;
+	// Variable normale : lettres, chiffres, underscore
+	while (str[start + len] != '\0' && bool == 1)
+	{
+		if (ft_isalnum(str[start + len]) == 0 && str[start + len] != '_')
+			bool = 0;
+		else
+			len++;
+	}
+	return (len);
+}
+
 int ft_is_dollar(char *str)
 {
     int i;
