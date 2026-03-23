99a47edf89acccbea106218896893841fa4bb7ca
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-23 15:25:55 +0100

    ajout des utils

---
+++ src/parsing/expand_utils.c
@@ -0,0 +1,32 @@
+#include "minishell.h"
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*result;
+
+	result = ft_strjoin(s1, s2);
+	free(s1);
+	return (result);
+}
+
+char	*ft_addchar(char *str, char c)
+{
+	char	*result;
+	int		len;
+	int		i;
+
+	len = ft_strlen(str);
+	result = malloc(len + 2);
+	if (result == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		result[i] = str[i];
+		i++;
+	}
+	result[i] = c;
+	result[i + 1] = '\0';
+	free(str);
+	return (result);
+}
