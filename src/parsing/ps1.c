0790651c68090b2ecf7c18eb433f926a1845dd77
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-23 15:25:29 +0100

    Ps1

--- src/parsing/ps1.c
+++ src/parsing/ps1.c
@@ -4,7 +4,6 @@ void	ft_ps1()
 {
 	char	*line;
 	t_list	*list;
-	t_token	*token;
 
 	line = readline("minibiendur$ ");
 	if (line == NULL)
@@ -24,12 +23,6 @@ void	ft_ps1()
   	}
 	// debut de la lecture de notre commande
 	list = ft_lexer(line);
-	token = (t_token *)list->content;
-	if (ft_is_dollar(token->value) == 1)
-	{
-		printf("Dollar trouvé!\n");
-		printf("Longueur var: %d\n", ft_var_len(token->value, 1));  // 1 = après le $
-	}
 	ft_print_tokens(list);
 	free(line);
 }
