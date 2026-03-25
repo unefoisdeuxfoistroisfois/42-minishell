d84bdb3d0479b269ee4c178ec7ba0712b938ed90
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-25 10:56:39 +0100

    parser2.c

---
+++ src/parsing/parser2.c
@@ -0,0 +1,59 @@
+#include "minishell.h"
+
+char	**ft_add_word(char **args, char *word)
+{
+	char	**new_args;
+	int		count;
+	int		i;
+
+	count = 0;
+	if (args != NULL)
+	{
+		while (args[count] != NULL)
+			count++;
+	}
+	new_args = malloc(sizeof(char *) * (count + 2));
+	if (new_args == NULL)
+		return (NULL);
+	i = 0;
+	while (i < count)
+	{
+		new_args[i] = args[i];
+		i++;
+	}
+	new_args[i] = ft_strdup(word);
+	new_args[i + 1] = NULL;
+	if (args != NULL)
+		free(args);
+	return (new_args);
+}
+
+void	ft_redir(t_cmd *cmd, t_list **list)
+{
+	t_token	*token;
+	t_token	*next_token;
+
+	token = (t_token *)(*list)->content;
+	*list = (*list)->next;
+	if (*list == NULL)
+		return ;
+	next_token = (t_token *)(*list)->content;
+	if (token->type == REDIR_IN)
+		cmd->infile = ft_strdup(next_token->value);
+	else if (token->type == REDIR_OUT)
+	{
+		cmd->outfile = ft_strdup(next_token->value);
+		cmd->append = 0;
+	}
+	else if (token->type == APPEND)
+	{
+		cmd->outfile = ft_strdup(next_token->value);
+		cmd->append = 1;
+	}
+	else if (token->type == HEREDOC)
+	{
+		cmd->delimiter = ft_strdup(next_token->value);
+		cmd->heredoc = 1;
+	}
+	*list = (*list)->next;
+}
