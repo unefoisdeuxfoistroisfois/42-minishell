f3e5e6548d8742eee97a4c4e492214d1c096c481
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-28 23:42:10 +0100

    Parsing

--- src/parsing/parser2.c
+++ src/parsing/parser2.c
@@ -6,24 +6,17 @@
 /*   By: britela- <britela-@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 17:03:46 by britela-          #+#    #+#             */
-/*   Updated: 2026/03/28 21:59:48 by britela-         ###   ########.fr       */
+/*   Updated: 2026/03/28 23:40:32 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**ft_add_word(char **args, char *word)
+static char	**ft_alloc_args(char **args, char *word, int count)
 {
 	char	**new_args;
-	int		count;
 	int		i;
 
-	count = 0;
-	if (args != NULL)
-	{
-		while (args[count] != NULL)
-			count++;
-	}
 	new_args = malloc(sizeof(char *) * (count + 2));
 	if (new_args == NULL)
 		return (NULL);
@@ -40,7 +33,22 @@ char	**ft_add_word(char **args, char *word)
 		return (NULL);
 	}
 	new_args[i + 1] = NULL;
+	return (new_args);
+}
+
+char	**ft_add_word(char **args, char *word)
+{
+	char	**new_args;
+	int		count;
+
+	count = 0;
 	if (args != NULL)
+	{
+		while (args[count] != NULL)
+			count++;
+	}
+	new_args = ft_alloc_args(args, word, count);
+	if (new_args && args)
 		free(args);
 	return (new_args);
 }
@@ -80,7 +88,9 @@ void	ft_redir(t_cmd *cmd, t_list **list)
 	token = (t_token *)(*list)->content;
 	*list = (*list)->next;
 	if (*list == NULL)
+	{
 		return ;
+	}
 	next_token = (t_token *)(*list)->content;
 	ft_set_redir(cmd, token, next_token);
 	*list = (*list)->next;
