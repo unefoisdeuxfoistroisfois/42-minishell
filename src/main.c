4051e294e064136825a2fa04c872ef864af0168b
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2026-03-28 16:05:19 +0100

    Main.c

--- src/main.c
+++ src/main.c
@@ -4,12 +4,15 @@ int	g_exit_status = 0;
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
 	(void)argv;
-	ft_setup_signals();
-	while (1)
+	if (argc == 1)
 	{
-		ft_ps1();
+		ft_setup_signals();
+		while (1)
+		{
+			ft_ps1();
+		}
+		return (0);
 	}
-	return (0);
+	return (-1);
 }
